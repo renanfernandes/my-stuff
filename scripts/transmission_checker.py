a1f9bff0f537b11d45240f1d25ccf4e6cadb2609
Renan Mathias Fernandes <me@renanfernandes.org>
2022-11-26 18:07:45 -0300

    changed the form to check if the download is complete

--- scripts/transmission_checker.py
+++ scripts/transmission_checker.py
@@ -10,8 +10,13 @@ def main():
     notification = pushover.Client()
     c = Client(username='transmission', password='transmission')
     for t in c.get_torrents():
-        if t.is_finished:
-            notification.send_message("Torrent completed: " + t.name)
+        print(dir(t))
+        print(t.progress)
+	#if t.is_finished:
+        #    print("finished "+ t.name)
+        #    notification.send_message("Torrent completed: " + t.name)
+        #else:
+        #    print("not finished" + t.name)
 
 
 if __name__ == '__main__':
