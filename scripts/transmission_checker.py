e702d56df7da6c042f4f2fb1933aadb40c8869a8
Renan Mathias Fernandes <renanfernandes@gatech.edu>
2022-11-26 12:24:10 -0500

    first version of the torrent checker using transmission-rpc

---
+++ scripts/transmission_checker.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+"""Check if torrent download has completed"""
+
+from transmission_rpc import Client
+import pushover
+
+
+def main():
+    notification = pushover.Client()
+    c = Client(username='transmission', password='transmission')
+    for t in c.get_torrents():
+        if t.isfinished:
+            notification.send_message("Torrent completed: " + t.name)
+
+
+if __name__ == '__main__':
+    main()
