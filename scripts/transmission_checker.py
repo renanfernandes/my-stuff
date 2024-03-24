d73318666a96dd4957452c69aa7715133b16e17d
Renan Mathias Fernandes <renanfernandes@gatech.edu>
2024-03-24 18:29:17 -0400

    Include text file to store latest notification

--- scripts/transmission_checker.py
+++ scripts/transmission_checker.py
@@ -6,18 +6,31 @@ from transmission_rpc import Client
 import pushover
 
 
+def read_last_notification():
+    try:
+        with open("last_notification.txt", "r") as file:
+            return file.read().strip()
+    except FileNotFoundError:
+        return None
+
+def write_last_notification(notification):
+    with open("last_notification.txt", "w") as file:
+        file.write(notification)
+
 def main():
-    #initiate the pushover and transmission-rpc objects
+    # Initialize the pushover and transmission-rpc objects
     notification = pushover.Client()
     c = Client(username='transmission', password='transmission')
 
     for t in c.get_torrents():
         if t.progress == 100.0:
-            print("finished "+ t.name)
-            notification.send_message("Torrent completed: " + t.name)
-        #else:
-        #    print("not finished" + t.name)
-
+            print("Finished " + t.name)
+            last_notification = read_last_notification()
+            if last_notification != t.name:
+                notification.send_message("Torrent completed: " + t.name)
+                write_last_notification(t.name)
+        # else:
+        #     print("Not finished " + t.name)
 
 if __name__ == '__main__':
     main()
